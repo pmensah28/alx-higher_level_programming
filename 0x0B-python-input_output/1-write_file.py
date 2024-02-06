80b9fdda239722acc06e7443c39f5fff63185adf
pmensah28 <princemensaheyram@gmail.com>
2024-02-06 17:10:32 +0000

    made correction in file 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,4 +13,4 @@ def write_file(filename="", text=""):
         The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)"
+        return f.write(text)
